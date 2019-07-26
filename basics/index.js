214a8fd7e406bf18b872dbe5c68a96d92e9798ed
david howard <dmh2000@users.noreply.github.com>
2019-07-26 15:30:59 -0700

    basic queries up to posts

--- basics/index.js
+++ basics/index.js
@@ -1,36 +1,99 @@
 import { GraphqlServer, GraphQLServer } from 'graphql-yoga';
 
+// scalar types - String, Boolean, Int, Float, ID
+
 // Type definitions : application schema
 const typeDefs = `
-type Query {
-  hello: String!,
-  name: String!
-  location: String!,
-  bio: String!
-}
+  type Query {
+    me : User!
+    post(index: Int): Post
+    postById(id: ID) : Post
+
+    greeting(name: String): String!
+    add(a: [Float!]): Float!
+    grades: [Int!]!
+
+    posts: [Post!]!
+  }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+  }
+
+  type Post {
+    id : ID!
+    title : String!
+    body : String!
+    published: Boolean!
+  }
 `;
+
+const posts = [
+  {
+    id: 1,
+    title: 'this is a post title 1',
+    body: 'This is a post body 1',
+    published: true,
+  },
+  {
+    id: 2,
+    title: 'this is a post title 2 ',
+    body: 'This is a post body 2 ',
+    published: true,
+  },
+  {
+    id: 3,
+    title: 'this is a post title 3 ',
+    body: 'This is a post body 3',
+    published: false,
+  },
+];
 
 // Resolvers
 const resolvers = {
   Query: {
-    hello() {
-      return 'hello';
+    me() {
+      return {
+        id: 1,
+        name: 'david howard',
+        email: 'xyz@xyz.com',
+        age: null,
+      };
     },
-    name() {
-      return 'my name';
+    post(parent, args, ctx, info) {
+      return posts[args.index];
     },
-    location() {
-      return 'sparks';
+    greeting(parent, args, ctx, info) {
+      console.log(args);
+      return `hello ${args.name}`;
     },
-    bio() {
-      return 'retired';
-    }
-  }
+    add(parent, args, ctx, info) {
+      let i = 1;
+      return args.a.reduce((p, v) => {
+        console.log(p, v, i++);
+        return p + v;
+      }, 0);
+    },
+    grades(parent, args, ctx, info) {
+      return [1, 2, 3, 4, 5];
+    },
+    posts(parent, args, ctx, info) {
+      return posts;
+    },
+    postById(parent, args, ctx, info) {
+      return posts.find(v => {
+        return v.id == args.id;
+      });
+    },
+  },
 };
 
 const server = new GraphQLServer({
   typeDefs: typeDefs,
-  resolvers: resolvers
+  resolvers: resolvers,
 });
 
 server.start(() => {
